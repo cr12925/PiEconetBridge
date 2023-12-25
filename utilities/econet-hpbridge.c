f303f0eabac0581fc35fa4711dbea795b7a4d8d4
Chris Royle <c@royle.org>
2023-12-25 20:36:00 +0000

    Fixed typo in pipe server config read

--- utilities/econet-hpbridge.c
+++ utilities/econet-hpbridge.c
@@ -5567,7 +5567,7 @@ int eb_readconfig(char *f)
 
 				if (!existing)	eb_debug (1, 0, "CONFIG", "Unable to create Pipe server device on %d.%d", net, stn);
 
-				existing->pipe.base = eb_malloc(__FILE__, __LINE__, "CONFIG", "Create pipe base string", strlen(eb_getstring(line, &matches[2])+1));
+				existing->pipe.base = eb_malloc(__FILE__, __LINE__, "CONFIG", "Create pipe base string", strlen(eb_getstring(line, &matches[2]))+1);
 
 				if (!existing->pipe.base)
 					eb_debug (1, 0, "CONFIG", "Unable to malloc() for pipe filename for station %d.%d", net, stn);
