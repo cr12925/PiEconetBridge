a737dd6b6649174332bcc498d4a9350f2da04add
Chris Royle <c@royle.org>
2024-05-12 19:20:38 +0100

    Fix fs_copy_to_cr() trailing space bug

--- utilities/fs.c
+++ utilities/fs.c
@@ -1084,7 +1084,11 @@ void fs_copy_to_cr(unsigned char *dest, unsigned char *src, unsigned short len)
 
 	*(dest+count) = '\0';	
 
-	while (strrchr(dest, ' ')) *(strrchr(dest, ' ')) = '\0'; // Get rid of trailing spaces
+	// This bit's broken
+	//while (strrchr(dest, ' ')) *(strrchr(dest, ' ')) = '\0'; // Get rid of trailing spaces
+	
+	while (dest[strlen(dest)-1] == ' ') dest[strlen(dest)-1] = '\0';
+
 }
 
 int fs_aun_send(struct __econet_packet_udp *p, int server, int len, unsigned short net, unsigned short stn)
@@ -9388,6 +9392,7 @@ void handle_fs_traffic (int server, unsigned char net, unsigned char stn, unsign
 						struct objattr oa;
 
 						fs_copy_to_cr(params, param, 255);
+
 
 						if (strchr(params, ' ') && sscanf(params, "%10s %80s", username, dir) == 2)
 						{
