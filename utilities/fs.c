1d65ecd92bdbe85c5026ac408dfb4eda70325254
Chris Royle <c@royle.org>
2024-06-15 11:15:33 +0100

    Adde debug to *LIB to track down RISC OS open directories issue

--- utilities/fs.c
+++ utilities/fs.c
@@ -9674,8 +9674,9 @@ void handle_fs_traffic (int server, unsigned char net, unsigned char stn, unsign
 								fs_store_tail_path(active[server][active_id].fhandles[n_handle].acorntailpath, p.acornfullpath);
 								active[server][active_id].fhandles[n_handle].mode = 1;
 
-								if (old > 0 && (old != active[server][active_id].current) && (old != active[server][active_id].lib))
+								if (old > 0 && (old != active[server][active_id].current) && (old != active[server][active_id].lib) && (old != active[server][active_id].root))
 								{
+									fs_debug (0, 2, "%12sfrom %3d.%3d Closing old user handle %d allocated for internal handle %d", "", net, stn, old, active[server][active_id].fhandles[old].handle);
 									fs_close_interlock(server, active[server][active_id].fhandles[old].handle, active[server][active_id].fhandles[old].mode);
 									fs_deallocate_user_dir_channel(server, active_id, old);
 								}
