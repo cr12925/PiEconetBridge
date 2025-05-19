4eef41458fb9b79e551e93bb968431492b15a3b2
Chris Royle <c@royle.org>
2025-05-19 22:15:01 +0100

    Updates to AUNProtocol.py

--- utilities/AUNProtocol.py
+++ utilities/AUNProtocol.py
@@ -52,12 +52,15 @@ FSOP_RESULT_INFO=4
 FSOP_RESULT_IAM=5
 FSOP_RESULT_SDISC=6
 FSOP_RESULT_DIR=7
-FSOP_RESULT_UNKNOWNCOMMAND=8
-FSOP_RESULT_NO_REPLY=-1
+FSOP_RESULT_UNKNOWN_COMMAND=8
+FSOP_RESULT_NOT_LISTENING=-1
+FSOP_RESULT_ERROR=-2
+FSOP_RESULT_NO_REPLY=-3
 
-# Transmission failures
-AUN_TX_RESULT_NOTLISTENING=-1
-AUN_TX_RESULT_NOADDRESS=-2
+# Transmission failures/results
+AUN_TX_RESULT_OK=0
+AUN_TX_RESULT_NOT_LISTENING=-1
+AUN_TX_RESULT_NO_ADDRESS=-2
 AUN_TX_RESULT_UNKNOWN=-255
 
 class AUNClient:
@@ -75,8 +78,14 @@ class AUNClient:
         self.ports_mutex = threading.Lock()
         self.ports = { }
         self.port_conditions = { }
+        self.port_callbacks = { }
+        self.port_doqueue = { }
         self.aunmap = { }
         self.hostmap = { }
+        self.acknak = { } # Dictionary of ACKs, NAKs, and IMMREPs received
+        self.acknak_condition = threading.Condition()
+        self.broadcast = { } # Dictionary of broadcasts
+        self.broadcast_condition = threading.Condition()
 
         # Find home
 
@@ -119,9 +128,33 @@ class AUNClient:
         self.aunmap_inverse = { v:k for k, v in self.aunmap.items() }
 
         self.listener_thread = threading.Thread(target=self.listener_code, args=())
+        self.listener_thread.daemon = True
         self.listener_thread.start()
 
+        self.garbage_collector = threading.Thread(target=self.gc, args=())
+        self.garbage_collector.daemon = True
+        self.garbage_collector.start()
+
         return None
+
+    def gc(self):
+        while True:
+            time.sleep(10)
+            self.acknak_condition.acquire()
+            if len(self.acknak) > 0:
+                for count in range(len(self.acknak)-1, 0):
+                    entry = self.acknak[count]
+                    if (time.monotonic() - entry[1]) > 2:
+                        self.acknak.pop(count)
+            self.acknak_condition.release()
+
+            self.broadcast_condition.acquire()
+            if len(self.broadcast) > 0:
+                for count in range(len(self.broadcast)-1, 0):
+                    entry = self.broadcast[count]
+                    if (time.monotonic() - entry[1]) > 2:
+                        self.broadcast.pop(conut)
+            self.broadcast_condition.release()
 
     def listener_code(self):
         while True:
@@ -138,23 +171,55 @@ class AUNClient:
             else:
                 traffic = f"--> {self.aun_typestr(packet[AUN_PTYPE]):>5} {packet[AUN_PORT]:02X}:{packet[AUN_CTRL]:02X} Seq {seq:08X}: {packet[8:]} from {net}.{stn} ({addr})"
             self.traffic_debug(traffic)
-            if (packet[AUN_PTYPE] == AUN_PT_IMM):
+
+            packet_type = packet[AUN_PTYPE]
+            packet_seq = seq
+            packet_port = packet[AUN_PORT]
+            packet_ctrl = packet[AUN_CTRL]
+
+            if (packet[AUN_PTYPE] == AUN_PT_ACK) or (packet[AUN_PTYPE] == AUN_PT_NAK or packet[AUN_PTYPE] == AUN_PT_IMMREP):
+                self.acknak_condition.acquire()
+                self.acknak[(net, stn, seq)] = (packet, time.monotonic())
+                self.acknak_condition.notify()
+                self.acknak_condition.release()
+            if (packet[AUN_PTYPE] == AUN_PT_BCAST):
+                self.broadcast_condition.acquire()
+                self.broadcast[(net, stn, seq)] = (packet, time.monotonic())
+                self.broadcast_condition.notify()
+                self.broadcast_condition.release()
+            elif (packet[AUN_PTYPE] == AUN_PT_IMM):
                 if (packet[AUN_CTRL] == 0x88): # Machinepeek
                     mp_reply = bytearray([(PEEK_HW & 0xff00) >> 8, ((PEEK_HW & 0xff)), (PEEK_VERS & 0xff00) >> 8, (PEEK_VERS & 0xff)])
                     mp_reply_aun = self.AUNPacket(AUN_PT_IMMREP, 0x00, 0x88, seq, mp_reply)
                     self.AUNTransmit(net, stn, mp_reply_aun)
+
             elif (packet[AUN_PTYPE] == AUN_PT_DATA): # Data - send ACK - we'll be more particular later!
-                if self.ports.get(packet[AUN_PORT]) != None:
-                    ack = self.AUNPacket(AUN_PT_ACK, packet[AUN_PORT], packet[AUN_CTRL], seq, bytearray([]))
-                    self.AUNTransmit(net, stn, ack)
-                    self.ports[packet[AUN_PORT]].append(packet)
-                    if self.port_conditions[packet[AUN_PORT]] != None:
-                        self.port_conditions[packet[AUN_PORT]].acquire()
-                        self.port_conditions[packet[AUN_PORT]].notify()
-                        self.port_conditions[packet[AUN_PORT]].release()
-                else:
-                    nak = self.AUNPacket(AUN_PT_NAK, packet[AUN_PORT], packet[AUN_CTRL], seq, bytearray([]))
+
+                if self.port_conditions[packet_port] != None:
+                    self.port_conditions[packet_port].acquire()
+
+                if self.ports.get(packet_port) == None and self.ports_callback.get(packet_port) == None:
+
+                    # Port not listening
+                    nak = self.AUNPacket(AUN_PT_NAK, packet_port, packet_ctrl, seq, bytearray([]))
                     self.AUNTransmit(net, stn, nak)
+
+                else:
+
+                    ack = self.AUNPacket(AUN_PT_ACK, packet_port, packet_ctrl, seq, bytearray([]))
+                    self.AUNTransmit(net, stn, ack)
+
+                    if self.port_doqueue.get(packet_port) and self.ports.get(packet_port) != None: # Queue exists and we've been asked to put traffic on the queue
+                        self.ports[packet_port].append(packet)
+                        if self.port_conditions[packet_port] != None:
+                            self.port_conditions[packet_port].notify()
+
+
+                    if self.port_callbacks.get(packet_port) != None:
+                        self.port_callbackis.get(packet_port) (net, stn, packet_port, packet_ctrl, packet_type, packet_seq, packet[8:])
+
+                if self.port_conditions[packet_port] != None:
+                    self.port_conditions[packet_port].release()
 
     def AUNPacket(self, ptype, port, ctrl, seq, data):
         header = bytearray([ptype, port, ctrl | 0x80, 0x00, (seq & 0xff), (seq & 0xff00) >> 8, (seq & 0xff0000) >> 16, (seq & 0xff000000) >> 24])
@@ -165,22 +230,45 @@ class AUNClient:
         netaddress, netport = self.AUNMapAddress(net, stn)
 
         if netaddress == None or netport == None:
-            return AUN_TX_RESULT_NO_ADDRESS, 0, 0
+            return AUN_TX_RESULT_NO_ADDRESS, None
 
         self.sent_time = time.time()
-        output = f"<-- {self.aun_typestr(packet[AUN_PTYPE]):>5} {packet[AUN_PORT]:02X}:{packet[AUN_CTRL]:02X} Seq {(packet[AUN_SEQ1] + (packet[AUN_SEQ2] << 8) + (packet[AUN_SEQ3] << 16) + (packet[AUN_SEQ4] << 24)):08X}: {packet[8:]} to {net}.{stn} ({netaddress}:{netport})"
+        seq = packet[AUN_SEQ1] + (packet[AUN_SEQ2] << 8) + (packet[AUN_SEQ3] << 16) + (packet[AUN_SEQ4] << 24)
+        output = f"<-- {self.aun_typestr(packet[AUN_PTYPE]):>5} {packet[AUN_PORT]:02X}:{packet[AUN_CTRL]:02X} Seq {seq:08X}: {packet[8:]} to {net}.{stn} ({netaddress}:{netport})"
         self.traffic_debug(output)
         packet[AUN_CTRL] &= 0x7f # Strip high bit
-        self.local_socket.sendto(packet, 0, (netaddress, netport))
+
+        tx_seq = seq
+        tx_count = 0
         self.seq += 4
-        if (packet[AUN_PTYPE] == AUN_PT_IMM):
-            # Await IMMREP or timeout
-            x=1
-            #print ("Would be waiting for IMMREP or timeout")
-        elif (packet[AUN_PTYPE] == AUN_PT_DATA):
-            # Await ACK or NAK or timeout
-            x=1
-            #print ("Would be waiting for ACK NAK or timeout")
+
+        result = AUN_TX_RESULT_NOT_LISTENING
+
+        while result != AUN_TX_RESULT_OK and tx_count < 6:
+            tx_count = tx_count + 1
+            self.local_socket.sendto(packet, 0, (netaddress, netport))
+            if (packet[AUN_PTYPE] == AUN_PT_BCAST or packet[AUN_PTYPE] == AUN_PT_IMMREP or packet[AUN_PTYPE] == AUN_PT_ACK or packet[AUN_PTYPE] == AUN_PT_NAK): # Don't look for a response
+                result = AUN_TX_RESULT_OK
+                ack_packet = bytearray()
+                continue
+                
+            self.acknak_condition.acquire()
+            if self.acknak_condition.wait(self.aun_timeout):
+                entry = self.acknak.get((net, stn, seq))
+                if entry:
+                    ack_packet = entry[0]
+                    if ack_packet[AUN_PTYPE] == AUN_PT_NAK:
+                        result = AUN_TX_RESULT_NOT_LISTENING
+                    elif ack_packet[AUN_PTYPE] == AUN_PT_ACK:
+                        result = AUN_TX_RESULT_OK
+                    elif ack_packet[AUN_PTYPE] == AUN_PT_IMMREP:
+                        result = AUN_TX_RESULT_OK
+            self.acknak_condition.release()
+
+        if result == AUN_TX_RESULT_OK:
+            return result, ack_packet
+        else:
+            return result, None
 
     def AUNUnMapAddress(self, addr):
 
@@ -236,7 +324,7 @@ class AUNClient:
 
     def FSOp(self, net, stn, OpNumber, data):
 
-        self.ReplyPort = self.DataPort()
+        self.ReplyPort = self.GetPort(None, True)
 
         handle_urd = handle_cwd = handle_lib = 0
 
@@ -250,8 +338,15 @@ class AUNClient:
         self.port_conditions[self.ReplyPort].acquire()
 
         packet = self.AUNPacket(AUN_PT_DATA, 0x99, 0x80, self.seq, self.op_data)
-        self.AUNTransmit(net, stn, packet)
+        result, ack = self.AUNTransmit(net, stn, packet)
  
+        if result != AUN_TX_RESULT_OK:
+            if result == AUN_TX_RESULT_NOT_LISTENING:
+                print ("Not listening")
+            else:
+                print ("Unknown error in transmission")
+            return FSOP_RESULT_ERROR, 0, None
+
         result_code = FSOP_RESULT_NO_REPLY
         error_code = 0xff
 
@@ -263,10 +358,11 @@ class AUNClient:
                 result_code = self.packet[AUN_DATA]
                 error_code = self.packet[AUN_DATA+1]
 
-                if error_code != 0:
-                    error_string = self.packet[AUN_DATA+2:].decode('ascii')
-                    print (f"Error &{error_code:02X}: {error_string}")
-                elif (result_code == FSOP_RESULT_LOAD):
+                #if error_code != 0:
+                    #error_string = self.packet[AUN_DATA+2:].decode('ascii')
+                    #print (f"Error &{error_code:02X}: {error_string}")
+                #elif (result_code == FSOP_RESULT_LOAD):
+                if (result_code == FSOP_RESULT_LOAD):
                     print ("Server has decoded that request as a *LOAD")
                 elif (result_code == FSOP_RESULT_SAVE):
                     print ("Server has decoded that request as a *SAVE")
@@ -282,23 +378,39 @@ class AUNClient:
                     self.handles[(net, stn)] = (urd_handle, cwd_handle, lib_handle)
                     self.debug (f"Logged in to {net}.{stn} with URD = {urd_handle:02X}, CWD = {cwd_handle:02X}, LIB = {lib_handle:02X}")
                 elif (result_code == FSOP_RESULT_SDISC):
+                    urd_handle = self.packet[AUN_DATA+2]
+                    cwd_handle = self.packet[AUN_DATA+3]
+                    lib_handle = self.packet[AUN_DATA+4]
+                    self.handles[(net, stn)] = (urd_handle, cwd_handle, lib_handle)
                     print ("Disc changed")
                 elif (result_code == FSOP_RESULT_DIR): # Dir handle change
                     current_handles = self.handles[(net, stn)]
                     self.handles[(net, stn)] = (current_handles[0], self.packet[AUN_DATA+2], current_handles[2])
                     self.debug (f"Changed working directory on {net}.{stn}, new URD handle = {self.packet[AUN_DATA+2]:02X}")
-                elif (result_code == FSOP_RESULT_UNKNOWNCOMMAND):
+                elif (result_code == FSOP_RESULT_UNKNOWN_COMMAND):
                     self.debug ("Unknown command within fileserver and cannot run programmes in this environment")
         else:
            self.packet = None
    
            self.port_conditions[self.ReplyPort].release()
    
-           self.put_port(self.ReplyPort)
+           self.PutPort(self.ReplyPort)
 
         return result_code, error_code, self.packet
 
-    def DataPort(self):
+    def report_FSOp_result(self, aun_client, fsop_result, fsop_error, packet):
+        if fsop_result < 0:
+            if fsop_result == FSOP_RESULT_NO_REPLY:
+                return "No reply"
+            elif fsop_result == FSOP_RESULT_NOT_LISTENING:
+                return "Not listening"
+        else:
+            if fsop_error != 0:
+                return f"{packet[10:].decode('ascii')}"
+            elif fsop_result == FSOP_RESULT_OK:
+                return "Success"
+
+    def GetPort(self, callback, enqueue = True):
     
         self.ports_mutex.acquire()
     
@@ -316,15 +428,35 @@ class AUNClient:
             else:
                 self.ports[self.port] = [ ]
                 self.port_conditions[self.port] = threading.Condition()
+                self.port_callbacks[self.port] = callback
+                self.port_doqueue[self.port] = enqueue
                 self.last_port = self.port
                 self.ports_mutex.release()
                 return self.port
      
-    def put_port(self, port):
+    def AUNBroadcastProcessor(self, net, stn, seq, packet):
+        traffic = f"--> {self.aun_typestr(packet[AUN_PTYPE]):>5} {packet[AUN_PORT]:02X}:{packet[AUN_CTRL]:02X} Seq {seq:08X}: {packet} from {net}.{stn} ({addr})"
+        self.traffic_debug(traffic)
+        # Insert other broadcast processing here
+
+    def AUNBroadcastHandler(self):
+
+        while True:
+            self.broadcast_condition.acquire()
+            self.broadcast_condition.wait()
+            for source, data in self.broadcast.items():
+                net, stn, seq = source
+                packet = data[0]
+                self.AUNBroadcastProcessor(net, stn, seq, packet)
+            self.broadcast_condition.release()
+
+    def PutPort(self, port):
         
         self.ports_mutex.acquire()
         self.ports.pop(port)
         self.port_conditions.pop(port)
+        self.port_callbacks.pop(port)
+        self.port_doqueue.pop(port)
         self.ports_mutex.release()
     
     
